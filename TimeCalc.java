1cfeb304f7f53e8d009f2104944f19bd56275475
nirduani1 <nirduani@icloud.com>
2024-11-15 15:15:31 +0200

    done

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,7 +4,6 @@ public class TimeCalc {
         int minutesToAdd = Integer.parseInt(args[1]);
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        if(hours>12){hours = hours-12;}
         int totalMinutes = hours*60 + minutes + minutesToAdd;
         int totalHours = totalMinutes/60;
         int newHours = totalHours%24;
