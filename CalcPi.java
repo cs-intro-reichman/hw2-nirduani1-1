7b16de798281b5ea0665e58c27abbd667a80f97b
nirduani1 <nirduani@icloud.com>
2024-11-16 01:24:08 +0200

    done

--- CalcPi.java
+++ CalcPi.java
@@ -2,7 +2,7 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int times = Integer.parseInt(args[0]);
 		double sum = 0.0;
-		for(int i = 0; i<=times; i++){
+		for(int i = 0; i<times; i++){
 			sum = sum + Math.pow(-1, i)*(1.0/(2*i+1));
 		}
 		System.out.println("pi according to java: "+ Math.PI);
