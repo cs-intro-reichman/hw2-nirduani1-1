5af5006aec2743802c2d34f1b07b8e6042232cdf
nirduani1 <nirduani@icloud.com>
2024-11-18 08:13:00 +0200

    done

--- TestRandom.java
+++ TestRandom.java
@@ -2,15 +2,20 @@ public  class  TestRandom {
 	public static void main(String[]  args) {
 		int t = Integer.parseInt(args[0]);
 		int big = 0; int small = 0; int i = 0;
-		do{double x = Math.random();
+
+	    do{double x = Math.random();
 		   i++;
 		   if(x>0.5){big = big +1;}
 		   else{small = small + 1;}}
-		   while (i<t); 
+		   while (i<t);
+
 		   System.out.println("> 0.5: " + big + " times");
 		   System.out.println("<= 0.5: " + small + " times");
+
 		   if((big != 0)&&(small != 0)){
+		   if(small<big){
 		   System.out.println("Ratio: " + (double)small/big);}
-		   
+		   else{System.out.println("Ratio: " + (double)big/small);}
+		   } 
 		}
 	}
