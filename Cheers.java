233662290ce9188a57c24d6fbda719c8a6641f06
nirduani1 <nirduani@icloud.com>
2024-11-18 17:15:01 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -8,8 +8,8 @@ public class Cheers {
                 for(int i = 0; i<upperCaseWord.length(); i++){
                         char letter = upperCaseWord.charAt(i);
                         if(problem.indexOf(letter) != -1){
-                              System.out.println("Give me a  " + letter + ": " + letter + "!");}
-                        else {System.out.println("Give me an " + letter + ": " + letter + "!");}
+                              System.out.println("Give me an " + letter + ": " + letter + "!");}
+                        else {System.out.println("Give me a  " + letter + ": " + letter + "!");}
                 }
                 
                         System.out.println("What does that spell?");
